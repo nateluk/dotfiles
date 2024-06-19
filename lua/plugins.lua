57a5874b33386fa9e1a0af082ce32f415ee867e7
Nathan Luk <nathan.luk@heykit.com.au>
2024-06-19 12:22:50 +1000

    fix sources and completions in lua. move lspconfig to module

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,34 @@
 return {
+  -- Neovim UI
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  'tpope/vim-surround',
+
+  -- LSP
+  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+  { 'williamboman/mason.nvim', config = true },
+  { 'williamboman/mason-lspconfig.nvim' },
+  { 'neovim/nvim-lspconfig' },
+  { 'L3MON4D3/LuaSnip' },
+  { 'hrsh7th/nvim-cmp' },
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  { 'saadparwaiz1/cmp_luasnip' },
+  { 'rafamadriz/friendly-snippets' },
 
   -- Editing tools
   'github/copilot.vim',
+  'tpope/vim-surround',
+
+  -- Other
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua', -- only load on lua files
+    opts = {
+      library = {
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+  { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
 }
